8aedc889a903130729ec4d441d943af23a130a31
Piera Marchesini <pieracarolina@yahoo.com.br>
2017-05-25 17:39:38 -0300

    Completed

--- MackCoreMotion/MackCoreMotion/ViewController.swift
+++ MackCoreMotion/MackCoreMotion/ViewController.swift
@@ -16,20 +16,25 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var pitchLabel: UILabel!
     @IBOutlet weak var yawLabel: UILabel!
     @IBOutlet weak var label: UILabel!
-    @IBOutlet var distance: UIView!
+    @IBOutlet weak var distance: UILabel!
+    
     
     let manager: CMMotionManager = CMMotionManager()
     let locationManager: CLLocationManager = CLLocationManager()
-    let range = 10
+    let range = 8
     
     //var roll: Double = 0
     //var pitch: Double = 0
     var yaw: Double = 0
     
     var point1: CLLocation = CLLocation(latitude: -23.54776382, longitude: -46.65116501)
-    var point2 = CLLocation(latitude: -23.54786682, longitude: -46.65027237)
-    var point3 = CLLocation(latitude: -23.547436460274149, longitude: -46.651246968901958)
+    var point2 = CLLocation(latitude: -23.54791260, longitude: -46.65043259)
+    var point3 = CLLocation(latitude: -23.54767609, longitude: -46.65094376)
     var currentLocalization: CLLocation? = nil
+    
+    var foundPoint1: Bool = false
+    var foundPoint2: Bool = false
+    var foundPoint3: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,6 +58,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                         //self.pitch = data.attitude.pitch * 180 / Double.pi
                         
                         self.yaw = data.attitude.yaw * 180 / Double.pi
+                        self.yawLabel.text = "Yaw: \(self.yaw) degrees"
                         
                         //Deixa tudo positivo de 0 grau até 360 graus
                         if self.yaw < 0 {
@@ -60,10 +66,28 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                             self.yawLabel.text = "Yaw: \(self.yaw) degrees"
                         }
                         
-                        if self.yaw < 358 && self.yaw > 280 {
-                            self.label.text = "TV da frente"
-                        } else {
-                            self.label.text = "Tudo mudou"
+                        if self.foundPoint1 {
+                            if self.yaw < 290 && self.yaw > 190 {
+                                self.label.text = "Prédio 33"
+                            } else {
+                                self.label.text = "Oh no! Jim is dead"
+                            }
+                        }
+                        
+                        if self.foundPoint2 {
+                            if self.yaw < 150 && self.yaw > 97 {
+                                self.label.text = "Entrada Consolação"
+                            } else {
+                                self.label.text = "Oh no! Jim is dead"
+                            }
+                        }
+                        
+                        if self.foundPoint3 {
+                            if self.yaw < 330 && self.yaw > 250 {
+                                self.label.text = "Mr. Cheney"
+                            } else {
+                                self.label.text = "Oh no! Jim is dead"
+                            }
                         }
                     }
                 }
@@ -74,18 +98,43 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         currentLocalization = manager.location
         
-        print("Latitude %.6f", currentLocalization?.coordinate.latitude)
-        print("Longitude %.6f", currentLocalization?.coordinate.longitude)
+        let distanceMeters = currentLocalization?.distance(from: point1)
+        let distanceMeters2 = currentLocalization?.distance(from: point2)
+        let distanceMeters3 = currentLocalization?.distance(from: point3)
         
-        let distanceMeters = currentLocalization?.distance(from: point3)
-        infoLabel.text = "\(Double((distanceMeters)!))"
+        self.view.backgroundColor = .red
         
-        if ((Double(distanceMeters!)) > Double(meters)){
-            view.backgroundColor = .red
-        }else{
+        distance.text = "\(Double((distanceMeters2)!))"
+        
+        if ((Double(distanceMeters!)) < Double(range)){
+            //Point 1
+            self.foundPoint1 = true
             view.backgroundColor = .green
+            distance.text = "\(Double((distanceMeters)!))"
+            
+            self.foundPoint2 = false
+            self.foundPoint3 = false
+            
+        } else if ((Double(distanceMeters2!)) < Double(range)){
+            //Point 2
+            self.foundPoint2 = true
+            view.backgroundColor = .green
+            distance.text = "\(Double((distanceMeters2)!))"
+            
+            self.foundPoint1 = false
+            self.foundPoint3 = false
+            
+        } else if ((Double(distanceMeters3!)) < Double(range)) {
+            //Point 3
+            self.foundPoint3 = true
+            view.backgroundColor = .green
+            distance.text = "\(Double((distanceMeters3)!))"
+            
+            self.foundPoint1 = false
+            self.foundPoint2 = false
         }
-        //print(distanceMeters)
+        
+        
     }
     
     
